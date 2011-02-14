d8f796019668ebe93b90d9d2f87dc56be45597e6
Stefan Gehn <stefan@srcbox.net>
2011-02-14 21:24:37 +0100

    Add ifdef-safeguard to generated configure.h

--- configure.h.cmake
+++ configure.h.cmake
@@ -1,7 +1,10 @@
+#ifndef GK_CONFIGURE_H
+#define GK_CONFIGURE_H
 #cmakedefine HAVE_GETADDRINFO
 #cmakedefine HAVE_GETHOSTBYNAME
 #cmakedefine HAVE_GNUTLS
 #cmakedefine HAVE_NTLM
 #cmakedefine HAVE_LIBSENSORS
 #cmakedefine HAVE_SSL
+#endif // GK_CONFIGURE_H
 
